4951140cbb68282d5754dec70c28d0df8437d7a5
yerin jeon <rin9682@daum.net>
2021-07-16 22:08:15 +0900

    help me...

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function collisionDetection() {
           b.status = 0;
           score++;
           if(score == brickRowCount*brickColumnCount) {
-            alert("YOU WIN, CONGRATS!");
+            alert("YOU WIN, YOU ARE MINE!");
             document.location.reload();
           }
         }
